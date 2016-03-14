5371cf2e522acb0295cfda6effde96c5441f1637
hendrikswan <hendrik.swanepoel@gmail.com>
2016-03-14 17:05:14 +0200

    starting with server side data

---
+++ data/generate.js
@@ -0,0 +1,16 @@
+module.exports = function(){
+    return {
+        auctions: [{
+            id: 1,
+            title: 'A bag of oranges',
+            description: 'A very big bag of lovely orange county oranges!',
+            amount: 100,
+        }],
+        auctions: [{
+            id: 2,
+            title: 'A bunch of tulips',
+            description: 'Between 6 and 9 tulips in a bunch!',
+            amount: 100,
+        }],
+    };
+};
--- src/main.js
+++ src/main.js
@@ -1,16 +1,70 @@
 import Rx from 'rx';
 import Cycle from '@cycle/core';
 import CycleDOM from '@cycle/dom';
+const {
+    div,
+    h2,
+    makeDOMDriver,
+} = CycleDOM;
+import labeledSlder from './labeledSlider';
 
-function main() {
-  return {
-    DOM: Rx.Observable.interval(1000)
-      .map(i => CycleDOM.h1(`${i} seconds elapsed`))
-  };
+function main(sources) {
+    const weightProps$ = Rx.Observable.of({
+        label: 'Weight',
+        unit: 'kg',
+        min: 40,
+        max: 150,
+        init: 70,
+    });
+
+    const weightSinks = labeledSlder({
+        DOM: sources.DOM, props: weightProps$,
+    });
+    const weightVTree$ = weightSinks.DOM;
+    const weightValue$ = weightSinks.value;
+
+    const heightProps$ = Rx.Observable.of({
+        label: 'Height',
+        unit: 'cm',
+        min: 140,
+        max: 220,
+        init: 170,
+    });
+
+    const heightSinks = labeledSlder({
+        DOM: sources.DOM, props: heightProps$,
+    });
+
+    const heightVTree$ = heightSinks.DOM;
+    const heightValue$ = heightSinks.value;
+
+    const bmi$ = Rx.Observable.combineLatest(weightValue$, heightValue$,
+        (weight, height) => {
+            const heightMeters = height * 0.01;
+            const bmi = Math.round(weight / (heightMeters * heightMeters));
+            return bmi;
+        }
+    );
+
+    const vtree$ = Rx.Observable.combineLatest(
+        bmi$,
+        weightVTree$,
+        heightVTree$,
+        (bmi, weightVTree, heightVTree) =>
+            div([
+                weightVTree,
+                heightVTree,
+                h2(`BMI is ${bmi}`),
+            ])
+    );
+
+    return {
+        DOM: vtree$,
+    };
 }
 
 const drivers = {
-  DOM: CycleDOM.makeDOMDriver('#app')
+    DOM: makeDOMDriver('#app'),
 };
 
 Cycle.run(main, drivers);
